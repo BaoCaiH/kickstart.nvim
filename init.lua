77de7f5659a2e374c714e02cdd8dafc8aba13a71
BaoCaiH <caihongbao280996@gmail.com>
2024-04-22 12:42:45 +0300

    Add rainbow csv

--- init.lua
+++ init.lua
@@ -184,6 +184,9 @@ require("lazy").setup({
     end,
   },
 
+  -- CSV beautify
+  "mechatroner/rainbow_csv",
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I"ve included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
